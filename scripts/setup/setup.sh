34cb97959efd92b3035e13a9e8dace64af076223
Jesse592 <jm.krijgsman@student.avans.nl>
2023-04-17 16:14:03 +0200

    Read NMEA messages in gps driver

--- scripts/setup/setup.sh
+++ scripts/setup/setup.sh
@@ -5,6 +5,7 @@ echo "Setting up enviroment..."
 sudo apt install python3 -y
 sudo apt install python3-pip -y
 pip install ds4drv
+pip install pynmea2
 
 # Installing rosserial-python
 echo "Setting up rosserial-python..."
@@ -15,19 +16,18 @@ sudo bash ./install_geographiclib_datasets.sh
 # creating workspace
 cd ..
 echo "Setting up workspace..."
-mkdir ds4_driver
-cd ds4_driver
+mkdir dependencies
+cd dependencies
 mkdir src
 catkin_make
 echo 'source '$PWD'/devel/setup.bash'>> ~/.bashrc
 
-
-# downloading driver 
+# downloading ds4_driver 
 cd src
 git clone "https://github.com/naoki-mizuno/ds4_driver.git"
 cd ds4_driver
 git checkout noetic-devel
-cd ../..
+cd ..
 
 # building driver
 echo "Building driver"
