1ed79c747e46f88b392fbd97ee34e62810d15e69
Raoul de Heer <raouldeheer@outlook.com>
2023-05-22 10:23:06 +0200

    Added services

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # araf-raspberrypi
+
+
+
+
+# Installing services
+
+copy services from ./scripts/services/ to /etc/systemd/system/
+```bash
+sudo systemctl daemon-reload
+sudo systemctl enable araf 
+sudo systemctl enable mavproxy 
+sudo systemctl start araf 
+sudo systemctl start mavproxy 
+```
+
--- scripts/deploy/compile.sh
+++ scripts/deploy/compile.sh
@@ -12,10 +12,11 @@ cd /home/araf/araf-raspberrypi
 
 # launch of the code
 echo "Starting launch.."
-rosnode kill -a
-source /home/araf/araf-raspberrypi/devel/setup.bash
-nohup roslaunch robotcontrol araf.launch </dev/null &>/dev/null&
-jobs
+sudo systemctl restart araf
+# rosnode kill -a
+# source /home/araf/araf-raspberrypi/devel/setup.bash
+# nohup roslaunch robotcontrol araf.launch </dev/null &>/dev/null&
+# jobs
 
 # succesfully flashed (-;
 echo "Succesfully deployed!"
