f0c5ac1ca2447fa36cf66d20d7448d228d878f55
Jesse592 <jm.krijgsman@student.avans.nl>
2023-05-30 13:29:01 +0200

    Remove send once action in manual control drive

--- src/robotcontrol/src/states/manualControlState.cpp
+++ src/robotcontrol/src/states/manualControlState.cpp
@@ -45,17 +45,15 @@ void ManualControlState::onControllerData(const ds4_driver::Status& msg, RobotCo
     int input_left = (int)(msg.axis_left_y * 100.0);
     int input_right = (int)(msg.axis_right_y * 100.0);
 
-    if (input_right != previous_right || input_left != previous_left)
-    {
-        ROS_INFO("Set drive forward: %d, rotate: %d", input_left, previous_right);
-        auto outmsg = geometry_msgs::Twist();
-        outmsg.linear.x = ((double)input_left / 100.0);
-        outmsg.linear.y = ((double)input_right / 100.0);
-        controller->drive_publisher.publish(outmsg);
+    ROS_INFO("Set drive forward: %d, rotate: %d", input_left, previous_right);
+    auto outmsg = geometry_msgs::Twist();
+    outmsg.linear.x = ((double)input_left / 100.0);
+    outmsg.linear.y = ((double)input_right / 100.0);
+    controller->drive_publisher.publish(outmsg);
 
-        previous_right = input_right;
-        previous_left = input_left;
-    }
+    previous_right = input_right;
+    previous_left = input_left;
+    
 }
 
 void ManualControlState::onRCOut(const geometry_msgs::Twist::ConstPtr &msg, RobotController *controller)
