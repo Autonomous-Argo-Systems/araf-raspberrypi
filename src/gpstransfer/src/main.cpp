d0ba7e8166060054c19e614cfded604adcd77847
Jesse592 <jm.krijgsman@student.avans.nl>
2023-06-01 16:27:27 +0200

    Move to NmeaTode library for gps

--- src/gpstransfer/src/main.cpp
+++ src/gpstransfer/src/main.cpp
@@ -1,5 +1,5 @@
 #include <ros/ros.h>
-#include <nmea/nmea.h>
+#include <nmeaparse/nmea.h>
 #include <libserial/SerialPort.h>
 #include <cstdlib>
 #include <cstring>
@@ -9,32 +9,26 @@
 #include <thread>
 #include <string.h>
 #include <chrono>
+#include <fstream>
+#include <iomanip>
 
-using namespace LibSerial ;
+using namespace LibSerial;
 
-
-constexpr const char* const SERIAL_PORT_UBLOX = "/dev/ttyUSB0";    
+constexpr const char *const SERIAL_PORT_UBLOX = "/dev/ttyACM0";
 SerialPort serialPort;
 
-nmeaINFO gpsInfo;
+nmea::NMEAParser parser;
+nmea::GPSService gps(parser);
+
 mavros_msgs::HilGPS gpsMessage;
 
-double ndegToDecimalUBLOX(double ndegValue)
+void readUBLOX()
 {
-  double degrees = std::floor(ndegValue / 100);     
-  double minutes = std::fmod(ndegValue, 100);          
-  double decimalDegrees = degrees + (minutes / 60);     
-  return decimalDegrees;
-}
 
-void readUBLOX(){
-    // Starting parser
-    nmeaPARSER parser;
-    nmea_zero_INFO(&gpsInfo);
-    nmea_parser_init(&parser);
-   
-    while (ros::ok()){
-        if (serialPort.GetNumberOfBytesAvailable() == 0){
+    while (ros::ok())
+    {
+        if (serialPort.GetNumberOfBytesAvailable() == 0)
+        {
             std::this_thread::sleep_for(std::chrono::milliseconds(10));
             continue;
         }
@@ -44,61 +38,71 @@ void readUBLOX(){
         serialPort.ReadLine(nmeaSentence, '\n', 10);
 
         // Parsing sentence
-        if (nmeaSentence[0] == '$'){
-            nmea_parse(&parser, nmeaSentence.c_str(), nmeaSentence.length(), &gpsInfo);
+        try
+        {
+            if (
+                nmeaSentence.find("GGA") != std::string::npos ||
+                nmeaSentence.find("GSA") != std::string::npos ||
+                nmeaSentence.find("GSV") != std::string::npos ||
+                nmeaSentence.find("RMC") != std::string::npos ||
+                nmeaSentence.find("VTG") != std::string::npos)
+            {
+                parser.readLine(nmeaSentence);
+            }
+        }
+        catch (nmea::NMEAParseError &e)
+        {
+            ROS_INFO("Error in parsing sentence %s\n%s", e.message.c_str(), e.nmea.text.c_str());
         }
     }
-
-    // Destroying parser if program quits
-    nmea_parser_destroy(&parser);
 }
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
     ros::init(argc, argv, "gpstransfer");
     ros::NodeHandle nh;
-    
+
     // Opening serial ports
     try
     {
         serialPort.Open(SERIAL_PORT_UBLOX);
     }
-    catch (const OpenFailed&)
+    catch (const OpenFailed &)
     {
         ROS_ERROR("The serial ports did not open correctly.");
-        return EXIT_FAILURE ;
+        return EXIT_FAILURE;
     }
 
     // Setting correct settings
     serialPort.SetBaudRate(BaudRate::BAUD_115200);
     serialPort.SetCharacterSize(CharacterSize::CHAR_SIZE_8);
-    serialPort.SetFlowControl(FlowControl::FLOW_CONTROL_NONE); 
+    serialPort.SetFlowControl(FlowControl::FLOW_CONTROL_NONE);
     serialPort.SetParity(Parity::PARITY_NONE);
     serialPort.SetStopBits(StopBits::STOP_BITS_1);
 
     // Starting reading ublox
-    std::thread ubloxThread (readUBLOX);
+    std::thread ubloxThread(readUBLOX);
 
     // Starting publiser
     ros::Publisher gpsPublisher = nh.advertise<mavros_msgs::HilGPS>("/mavros/hil/gps", 1000);
 
     // Publishing to px4
     ros::Rate loop_rate(10);
-    while (ros::ok()) {
+    while (ros::ok())
+    {
         // Creating message
-        gpsMessage.geo.latitude = ndegToDecimalUBLOX(gpsInfo.lat);
-        gpsMessage.geo.longitude = ndegToDecimalUBLOX(gpsInfo.lon);
-        gpsMessage.geo.altitude = gpsInfo.elv;
-        gpsMessage.eph = gpsInfo.HDOP / 100.0f;
-        gpsMessage.epv = gpsInfo.VDOP / 100.0f;
-        gpsMessage.cog = gpsInfo.direction;
-        gpsMessage.fix_type = gpsInfo.fix;
-        gpsMessage.vel = gpsInfo.speed / 3.6f;
-        gpsMessage.satellites_visible = gpsInfo.satinfo.inview;
+        gpsMessage.geo.latitude = gps.fix.latitude;
+        gpsMessage.geo.longitude = gps.fix.longitude;
+        gpsMessage.geo.altitude = gps.fix.altitude;
+        gpsMessage.eph = gps.fix.horizontalDilution * 100;
+        gpsMessage.epv = gps.fix.verticalDilution * 100;
+        gpsMessage.cog = gps.fix.travelAngle;
+        gpsMessage.fix_type = gps.fix.type;
+        gpsMessage.vel = (gps.fix.speed / 3.6f) * 100;
+        gpsMessage.satellites_visible = gps.fix.visibleSatellites;
 
         // Printing debug
-        ROS_INFO("Latitude: %8f, Longtitude: %8f, Fix: %d, Visible: %d", 
-        gpsMessage.geo.latitude, gpsMessage.geo.longitude,
-         gpsMessage.fix_type, gpsMessage.satellites_visible);
+        ROS_INFO("%s", gps.fix.toString().c_str());
 
         // Sending message
         gpsPublisher.publish(gpsMessage);
