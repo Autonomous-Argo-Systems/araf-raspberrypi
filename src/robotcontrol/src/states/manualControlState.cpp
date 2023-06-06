16e2eaf498c66fc5ed92c8a562cb0dc9156753ea
Jesse592 <jm.krijgsman@student.avans.nl>
2023-06-06 07:44:45 +0200

    Fix double input check

--- src/robotcontrol/src/states/manualControlState.cpp
+++ src/robotcontrol/src/states/manualControlState.cpp
@@ -41,18 +41,21 @@ void ManualControlState::onControllerData(const ds4_driver::Status& msg, RobotCo
         ROS_INFO("Switching to auto mode");
         controller->switchState(autonomousControlState);
     }
-
+    
     int input_left = (int)(msg.axis_left_y * 100.0);
     int input_right = (int)(msg.axis_right_y * 100.0);
 
-    ROS_INFO("Set drive forward: %d, rotate: %d", input_left, previous_right);
-    auto outmsg = geometry_msgs::Twist();
-    outmsg.linear.x = ((double)input_left / 100.0);
-    outmsg.linear.y = ((double)input_right / 100.0);
-    controller->drive_publisher.publish(outmsg);
+    if (input_right != previous_right || input_left != previous_left)   
+    {
+        ROS_INFO("Set drive forward: %d, rotate: %d", input_left, previous_right);
+        auto outmsg = geometry_msgs::Twist();
+        outmsg.linear.x = ((double)input_left / 100.0);
+        outmsg.linear.y = ((double)input_right / 100.0);
+        controller->drive_publisher.publish(outmsg);
 
-    previous_right = input_right;
-    previous_left = input_left;
+        previous_right = input_right;
+        previous_left = input_left;
+    }
     
 }
 
