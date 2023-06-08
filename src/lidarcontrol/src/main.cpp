1ef377ca08dac9f923d57adc065a9bb21d09c624
Jesse592 <jm.krijgsman@student.avans.nl>
2023-06-08 12:29:30 +0200

    Add config to lidar settings

--- src/lidarcontrol/src/main.cpp
+++ src/lidarcontrol/src/main.cpp
@@ -9,14 +9,15 @@ sensor_msgs::PointCloud2 pointcloud_valid;
 ros::Publisher points_pub;
 ros::Publisher risk_pub;
 
+BoxValidator bbVal;
+BasicAnalyzer an;
+
 void onLidarData(const sensor_msgs::PointCloud2 pointcloud)
 {
     // Filter out all points not on route
-    BoxValidator bbVal;
     bbVal.Validate(pointcloud, pointcloud_valid);
 
     // Calculate risk from validated points
-    BasicAnalyzer an;
     float risk = an.Analyze(pointcloud_valid);
     std_msgs::Float32 msg;
     msg.data = risk;
@@ -25,15 +26,37 @@ void onLidarData(const sensor_msgs::PointCloud2 pointcloud)
     risk_pub.publish(msg);
 }
 
+std::vector<float> convertToArray(std::string value)
+{
+    std::stringstream ss(value);
+    std::istream_iterator<std::string> begin(ss);
+    std::istream_iterator<std::string> end;
+    std::vector<std::string> tokens(begin, end);
+    std::vector<float> floats;
+
+    for (auto &s: tokens) {
+        floats.push_back(std::stof(s));
+    }
+ 
+    return floats;
+}
+
 int main(int argc, char **argv){
     ros::init(argc, argv, "lidarcontrol");
     ros::NodeHandle nh;
     
+    std::string validatorConfigParam, analyzerConfigParam;
+
+    ros::param::get("~lidarcontrol/validator_config", validatorConfigParam);
+    ros::param::get("~lidarcontrol/analyzer_config", analyzerConfigParam);    
+
     points_pub = nh.advertise<sensor_msgs::PointCloud2>("validated_points", 1000);
     risk_pub = nh.advertise<std_msgs::Float32>("lidar_calculated_risk", 1000);
+
     ros::Subscriber rcin_sub = nh.subscribe<sensor_msgs::PointCloud2>("velodyne_points", 10, onLidarData);
 
-    ROS_INFO("Initialized lidarcontrol");
+    bbVal.Configure(convertToArray(validatorConfigParam));
+    an.Configure(convertToArray(analyzerConfigParam));
 
     ros::Rate loop_rate(1000);
     while (ros::ok()) {
